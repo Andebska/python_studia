f598606bf546119751ec2164a4b30485d24294c7
Anna Dębska <149374274+Andebska@users.noreply.github.com>
2025-01-10 13:10:56 +0100

    Update test.py

--- projekt_zaliczeniowy/test.py
+++ projekt_zaliczeniowy/test.py
@@ -2,9 +2,17 @@ import unittest
 from main import *
 
 class TestPolynomial(unittest.TestCase):
+    def test_invalid_input(self):
+        with self.assertRaises(ValueError):
+            Polynomial("str")
+        with self.assertRaises(ValueError):
+            Polynomial([1, "str", 3])
+
     def test_degree(self):
-        p = Polynomial([1, 0, 2])     # 1 + 2x^2
-        self.assertEqual(p.degree(),2)
+        p1 = Polynomial([1, 0, 2])     # 1 + 2x^2
+        p2 = Polynomial([0])
+        self.assertEqual(p1.degree(),2)
+        self.assertEqual(p2.degree(), 0)
 
     def test_is_zero(self):
         p = Polynomial([0, 0, 0])
@@ -21,12 +29,19 @@ class TestPolynomial(unittest.TestCase):
         self.assertNotEqual(p1, p2)
 
     def test_str(self):
-        p = Polynomial([1, 2, 3])
-        self.assertEqual(str(p), "3x^2 + 2x^1 + 1")
+        p1 = Polynomial([1, 2, 3])
+        p2 = Polynomial([-1, -3, 4])
+        p3 = Polynomial([0, 0, 3])
+        self.assertEqual(str(p1), "3x^2 + 2x + 1")
+        self.assertEqual(str(p2), "4x^2 - 3x - 1")
+        self.assertEqual(str(p3), "3x^2")
 
     def test_getitem(self):
         p = Polynomial([5, 3, 7])       # 5 + 3x + 7x^2
-        self.assertEqual(p.__getitem__(2), 7)
+        self.assertEqual(p[2], 7)
+        self.assertEqual(p[100], 0)
+        with self.assertRaises(IndexError):
+            _ = p[-5]
 
     def test_add(self):
         p1 = Polynomial([1, 2, 3])     # 1 + 2x + 3x^2
@@ -41,11 +56,17 @@ class TestPolynomial(unittest.TestCase):
     def test_mul(self):
         p1 = Polynomial([1, 1])      # 1 + x
         p2 = Polynomial([1, -1])     # 1 - x
+        p3 = Polynomial([0])
         self.assertEqual(p1 * p2, Polynomial([1, 0, -1]))
+        self.assertEqual(p1 * p3, Polynomial([0]))
 
     def test_call(self):
-        p = Polynomial([1, 2, 3])   # 1 + 2x + 3x^2
-        self.assertEqual(p(2), 17)
+        p1 = Polynomial([1, 2, 3])   # 1 + 2x + 3x^2
+        p2 = Polynomial([-1, -2, 3])    # -1 -2x + 3x^2
+        self.assertEqual(p1(0), 1)
+        self.assertEqual(p1(2), 17)
+        self.assertEqual(p2(0), -1)
+        self.assertEqual(p2(2), 7)
 
 
 if __name__ == '__main__':
